b4ee7282b2f2d8f41c102bfbaa5ad6c706d509fd
Sevinda Perera <sevindacharukaperera@gmail.com>
2024-06-18 23:36:37 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+// username = scp-bookstore
+// password = bookstore-password
+
+export const mongoDBURL =
+  "mongodb+srv://scp-bookstore:bookstore-password@bookstore.3btdryh.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Bookstore";
