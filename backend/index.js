efa5cdcd1629f40648ed70bfe78a17a0a8312eb2
Sevinda Perera <sevindacharukaperera@gmail.com>
2024-06-19 22:37:19 +0530

    Add endpoint to update a book by ID

--- backend/index.js
+++ backend/index.js
@@ -60,6 +60,31 @@ app.get("/book/:id", async (req, res) => {
   }
 });
 
+// UPDATING A BOOK USING ID (PUT)
+app.put("/book/:id", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res.status(400).send({
+        message: "Send all required fields: title, author, and publishYear",
+      });
+    }
+
+    const { id } = req.params;
+
+    const result = await Book.findByIdAndUpdate(id, req.body);
+
+    if (!result) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+    return res.status(200).json({message: "Book updated successfully"});
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+
+
 mongoose
   .connect(mongoDBURL)
   .then(() => {
