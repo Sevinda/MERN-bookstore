191d92738534c123672511177d4cbd2292f0e836
Sevinda Perera <sevindacharukaperera@gmail.com>
2024-06-19 22:08:47 +0530

    Posting a book to DB function created

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,35 @@ import express from "express";
 import mongoose from "mongoose";
 
 import { mongoDBURL, PORT } from "./config.js";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
+app.use(express.json())
 
 app.get("/", (req, res) => {
-  return res.status(234).send("Welcome codeandrender");
+  return res.status(234).send("Welcome to codeandrender bookstore");
+});
+
+app.post("/books", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res.status(400).send({
+        message: "Send all required fields: title, author, publishYear",
+      });
+    }
+    const newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      publishYear: req.body.publishYear,
+    };
+
+    const book = await Book.create(newBook);
+
+    return res.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).send({ message: error.message });
+  }
 });
 
 mongoose
