78eb52888aa8d6090587cb6b7c08c9fea462f3a4
Sevinda Perera <sevindacharukaperera@gmail.com>
2024-06-20 22:35:02 +0530

    Update react-router-dom and created main pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+import {CreateBook, DeleteBook, EditBook, Home, ShowBooks} from "./pages/pagesPaths.js"
 
 const App = () => {
   return (
-    <div>
-      <h1 className="font-bold underline text-[40px]">Hello Scp</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/books/create" element={<CreateBook />} />
+      <Route path="/books/details/:id" element={<ShowBooks />} />
+      <Route path="/books/edit/:id" element={<EditBook />} />
+      <Route path="/books/delete/:id" element={<DeleteBook />} />
+    </Routes>
   );
 };
 
---
+++ frontend/src/pages/pagesPaths.js
@@ -0,0 +1,7 @@
+import CreateBook from "./CreateBook";
+import DeleteBook from "./DeleteBook";
+import EditBook from "./EditBook";
+import Home from "./Home";
+import ShowBooks from "./ShowBooks";
+
+export { CreateBook, DeleteBook, EditBook, Home, ShowBooks };
